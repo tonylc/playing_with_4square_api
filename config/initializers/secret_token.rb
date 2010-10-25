843d86612b43d6b2064b59ea466f3c61583bbe8e
Tony <tonylc@gmail.com>
2010-10-24 21:27:04 -0700

    first commit with rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PlayingWith4squareApi::Application.config.secret_token = '6f4e1ed8debb66b7fe2199ad3758b1565d3f33abd91b843d267523526aea1d3a9af1c19ea5f54f06f3dbf578fd9712e54550fb0e3d8d232dcdb1f5f8c1d121cf'
